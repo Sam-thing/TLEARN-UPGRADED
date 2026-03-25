583b78ed67017d148fa47594e692c1e45673e920
Julius-696 <mainajulius696@gmail.com>
2026-03-25 12:53:39 +0300

    FIXI SESS ROUTES

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -1,6 +1,7 @@
 // server/routes/sessions.js - Session Routes
 import express from 'express';
 import multer from 'multer';
+import { catchAsync } from '../middleware/errorHandler.js';
 import path from 'path';
 import { protect } from '../middleware/auth.js';
 import Session from '../models/Session.js';
