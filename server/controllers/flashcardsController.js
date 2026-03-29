8bc92d735bb8615794b2670216b25383d5b1169e
Julius-696 <mainajulius696@gmail.com>
2026-03-30 00:30:53 +0300

    adding gamifications

--- server/controllers/flashcardsController.js
+++ server/controllers/flashcardsController.js
@@ -3,6 +3,8 @@ import Flashcard from '../models/Flashcard.js';
 import Topic from '../models/Topic.js';
 import Note from '../models/Note.js';
 import aiService from '../services/aiService.js';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { gamificationService } from '../services/gamificationService.js';
 import { catchAsync } from '../middleware/errorHandler.js';
 
 /**
