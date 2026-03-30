b21432bded3a013589a4f9bad1fa392175586054
Julius-696 <mainajulius696@gmail.com>
2026-03-30 20:38:04 +0300

    adding more time to Axios

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'https://tlearnapp.onrender.com/api' || 'http://localhost:3000/api',
   withCredentials: true,
-  timeout: 15000,
+  timeout: 60000,
 });
 
 // Request interceptor - Add Authorization token
